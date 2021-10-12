d49c7b1e0ae82e2000a47f9421437d872896ad6b
KRASAVA <vladimirova.a-a@yandex.ru>
2021-10-13 01:27:35 +0400

    Init

---
+++ src/colors.ts
@@ -0,0 +1,31 @@
+import {BackgroundColors, backgroundColors, Effects, effects, FontColors, fontColors, Reset} from './model';
+import {ColorOptions} from './model';
+
+function addColor(text: string, color: FontColors | BackgroundColors, isBackground = false) {
+    if (isBackground) {
+        return text + backgroundColors[color];
+    }
+    return text + fontColors[color];
+}
+function getEffects(effectList: Effects) {
+    return effectList.map(effect => effects[effect]).join('');
+}
+export function color(text: string, options: ColorOptions) {
+    const preparedText = text.replace(/ё/g, 'е');
+    let result = '';
+    if (options) {
+        if (options.font) {
+            result = addColor(result, options.font);
+        }
+        if (options.background) {
+            result = addColor(result, options.background, true);
+        }
+        if (options.effects) {
+            result += getEffects(options.effects);
+        }
+        result += preparedText;
+        result += Reset;
+        return result;
+    }
+    return preparedText;
+}
---
+++ src/model.ts
@@ -0,0 +1,60 @@
+type StringKeyValue =  {
+    [key: string]: string
+}
+
+export const Reset: string = '\x1b[0m';
+export const effects: StringKeyValue = {
+    bright: '\x1b[1m',
+    dim: '\x1b[2m',
+    italic: '\x1b[3m',
+    underscore: '\x1b[4m',
+    blink: '\x1b[5m',
+};
+export const fontColors: StringKeyValue = {
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+};
+export const backgroundColors: StringKeyValue = {
+    black: '\x1b[40m',
+    red: '\x1b[41m',
+    green: '\x1b[42m',
+    yellow: '\x1b[43m',
+    blue: '\x1b[44m',
+    magenta: '\x1b[45m',
+    cyan: '\x1b[46m',
+    white: '\x1b[47m',
+};
+export const contrast: StringKeyValue = {
+    black: 'white',
+    red: 'black',
+    green: 'black',
+    yellow: 'black',
+    blue: 'black',
+    magenta: 'black',
+    cyan: 'black',
+    white: 'black',
+};
+
+export type Effects = Array<keyof typeof effects>;
+export type FontColors = keyof typeof fontColors;
+export type BackgroundColors = keyof typeof backgroundColors;
+export type Contrast = keyof typeof contrast;
+
+export type ColorOptions = {
+    font?: FontColors;
+    background?: BackgroundColors;
+    effects?: Effects;
+}
+
+export type MarkdownOptions = {
+    bold?: boolean;
+    italic?: boolean;
+    mono?: boolean;
+    link?: string;
+}
